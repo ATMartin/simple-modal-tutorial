32025863f7bdd251f532708d9d4d2d3036c06f83
Alex Martin <martin.alex.t+github@gmail.com>
2015-03-25 00:21:45 -0400

    Add openModal method and assign target to button

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    openModal: function(target) {
+      var modal = Ember.View.views[target];
+      modal.send('toggleModal');
+    }
+  }
 });
