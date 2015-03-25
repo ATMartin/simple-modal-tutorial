ce7be6ef491f38c435b6767a01b33f143dcb6d02
Alex Martin <martin.alex.t+github@gmail.com>
2015-03-24 22:48:18 -0400

    Add toggleModal action

--- app/components/simple-modal.js
+++ app/components/simple-modal.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
+import layout from '../templates/components/simple-modal';
 
 export default Ember.Component.extend({
+  actions: {
+    toggleModal: function() {
+      this.set('enabled', !this.get('enabled'));
+    }
+  },
+  layout: layout
 });
