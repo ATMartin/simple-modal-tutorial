4b8b3b5192da4c2d6102bc128bb4bdc579d58201
Alex Martin <martin.alex.t+github@gmail.com>
2015-07-10 03:39:15 -0400

    Upgrade for Ember 1.13.3 / 2.0.0-beta.1

--- app/components/simple-modal.js
+++ app/components/simple-modal.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 import layout from '../templates/components/simple-modal';
 
 export default Ember.Component.extend({
+  expose: function() {
+    var my_controller = this.get('targetObject');
+    var exposedName = "comp-" + this.get('id');
+    my_controller.set(exposedName, this);
+  }.on('init'), 
   actions: {
     toggleModal: function() {
       this.toggleProperty('enabled');
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     openModal: function(target) {
-      var modal = Ember.View.views[target];
+      // "Ember.View" is deprecated and the views hash no longer exists.
+      // var modal = Ember.View.views[target];
+      var modal = this.get('comp-' + target); 
       modal.send('toggleModal');
     }
   }
